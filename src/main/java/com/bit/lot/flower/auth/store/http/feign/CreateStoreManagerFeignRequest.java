9520a92ea8edb372aa54780580f7c53c9e32b252
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-01 21:07:14 +0900

    fix: fix the CreateStoreManagerFeignRequest URL

--- src/main/java/com/bit/lot/flower/auth/store/http/feign/CreateStoreManagerFeignRequest.java
+++ src/main/java/com/bit/lot/flower/auth/store/http/feign/CreateStoreManagerFeignRequest.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("create-store-manager")
+@FeignClient(value = "create-store-manager", url = "${service.user.domain}")
 public interface CreateStoreManagerFeignRequest {
   @RequestMapping(method = RequestMethod.POST, value = "/users/store-manager")
   ResponseEntity<String> create(CreateStoreMangerCommand createStoreMangerCommand);
