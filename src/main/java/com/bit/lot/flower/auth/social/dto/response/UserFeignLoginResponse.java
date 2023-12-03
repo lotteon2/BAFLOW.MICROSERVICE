150880e0db2056bb9d913a86062a0b0343c6d04b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-03 11:30:56 +0900

    add the profile image in social user login response

--- src/main/java/com/bit/lot/flower/auth/social/dto/response/UserFeignLoginResponse.java
+++ src/main/java/com/bit/lot/flower/auth/social/dto/response/UserFeignLoginResponse.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class UserFeignLoginResponse{
+  private String profileImage;
   private String nickname;
   private boolean isPhoneNumberIsRegistered;
   private final String message ="유저 가져오기 성공";
