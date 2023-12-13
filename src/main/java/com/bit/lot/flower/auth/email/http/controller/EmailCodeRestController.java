100aa91e1523871afae709d60500bd065eee2048
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-13 11:24:44 +0900

    fix the kakao App key

--- src/main/java/com/bit/lot/flower/auth/email/http/controller/EmailCodeRestController.java
+++ src/main/java/com/bit/lot/flower/auth/email/http/controller/EmailCodeRestController.java
@@ -6,13 +6,10 @@ import com.sun.istack.NotNull;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,13 +18,13 @@ public class EmailCodeRestController {
 
   private final EmailCodeService emailCodeService;
 
-  @PostMapping("/api/auth/emails/{email}")
+  @PostMapping("/emails/{email}")
   public ResponseEntity<String> create(@NotNull @PathVariable String email) {
     emailCodeService.create(email);
     return ResponseEntity.ok("이메일이 성공적으로 보내졌습니다.");
   }
 
-  @PatchMapping("/api/auth/emails/{email}")
+  @PatchMapping("/emails/{email}")
   public ResponseEntity<String> verify(@PathVariable String email, @Valid  @RequestBody VerifyCodeDto dto) {
     emailCodeService.verify(email,dto.getCode());
     return ResponseEntity.ok("이메일이 성공적으로 보내졌습니다.");
