2e87a6ca5800b1b7d903fe7bb2dbfab9ae3b3336
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-14 10:30:04 +0900

    fix: delete the OauthController

--- src/main/java/com/bit/lot/flower/auth/oauth/OauthController.java
+++ src/main/java/com/bit/lot/flower/auth/oauth/OauthController.java
@@ -1,14 +1,8 @@
 package com.bit.lot.flower.auth.oauth;
 
-import com.bit.lot.flower.auth.common.valueobject.AuthenticationProvider;
 import com.bit.lot.flower.auth.oauth.facade.OauthLoginAccessTokenRequestFacade;
 import com.bit.lot.flower.auth.oauth.facade.OauthUserMeInfoRequestFacade;
-import com.bit.lot.flower.auth.social.dto.command.SocialLoginRequestCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -20,11 +14,11 @@ public class OauthController {
   private final OauthLoginAccessTokenRequestFacade oauthLoginRequestFacade;
   private final OauthUserMeInfoRequestFacade userMeInfoRequestFacade;
 
-  @GetMapping("/login/oauth2/code/{provider}")
-  public ResponseEntity<SocialLoginRequestCommand> requestSocialInfo(@RequestParam String code, @PathVariable
-      AuthenticationProvider provider){
-      oauthLoginRequestFacade.request(provider,code);
-    return null;
-  }
+//  @GetMapping("/login/oauth2/{provider}")
+//  public ResponseEntity<SocialLoginRequestCommand> requestSocialInfo(@RequestParam String code, @PathVariable
+//      AuthenticationProvider provider){
+//      oauthLoginRequestFacade.request(provider,code);
+//    return null;
+//  }
 
 }
