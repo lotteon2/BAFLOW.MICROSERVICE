7ddacffec7ff1129c9312bb742ccd27cd863d000
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-14 21:27:05 +0900

    add: add the cors at webConfig

--- src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
+++ src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
@@ -44,6 +44,8 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("http://localhost:3001")
+        .allowedOrigins("http://localhost:3002")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .exposedHeaders("Set-Cookie")
