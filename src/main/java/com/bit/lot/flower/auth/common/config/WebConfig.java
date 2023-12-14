f4a3819ae35cb42eee1f466f075e8e7106aca243
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-14 09:36:30 +0900

    fix: register RestTemplate as bean

--- src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
+++ src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.bit.lot.flower.auth.common.security.TokenHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,16 +18,21 @@ public class WebConfig implements WebMvcConfigurer {
   private final TokenHandler tokenHandler;
 
   @Bean
-  public CommonLogoutInterceptor commonLogoutInterceptor(){
+  public CommonLogoutInterceptor commonLogoutInterceptor() {
     return new CommonLogoutInterceptor(tokenHandler
     );
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(commonLogoutInterceptor())
         .excludePathPatterns(
-           "/swagger-ui.html",
+            "/swagger-ui.html",
             "/swagger-resources/**",
             "/v2/api-docs",
             "/webjars/**"
