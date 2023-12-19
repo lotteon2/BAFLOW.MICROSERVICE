e84e75fb7d804a45f779f3ea06abba0962b86a62
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-19 20:25:15 +0900

    refactor: refactor the config

--- src/main/java/com/bit/lot/flower/auth/common/config/RedisConfig.java
+++ src/main/java/com/bit/lot/flower/auth/common/config/RedisConfig.java
@@ -1,21 +1,16 @@
 package com.bit.lot.flower.auth.common.config;
 
-import javax.annotation.PostConstruct;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@NoArgsConstructor
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
 
   @Value("${spring.redis.host}")
@@ -28,24 +23,27 @@ public class RedisConfig {
 
 
   @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
-        host, port);
+  RedisConnectionFactory jedisConnectionFactory() {
+       RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
     redisStandaloneConfiguration.setHostName(host);
     redisStandaloneConfiguration.setPort(port);
     redisStandaloneConfiguration.setPassword(password);
-    return new JedisConnectionFactory(redisStandaloneConfiguration);
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate(
-      JedisConnectionFactory jedisConnectionFactory) {
+      RedisConnectionFactory redisConnectionFactory) {
     RedisTemplate<String, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(jedisConnectionFactory);
+    template.setConnectionFactory(redisConnectionFactory);
     template.setKeySerializer(new StringRedisSerializer());
     template.setValueSerializer(new StringRedisSerializer());
     template.setHashKeySerializer(new GenericToStringSerializer<>(String.class));
     template.setHashValueSerializer(new GenericToStringSerializer<>(String.class));
     return template;
   }
+
+
+
+
 }
