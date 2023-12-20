8887cde6b5ed95771caa017f3920341a73adc373
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-20 20:15:24 +0900

    fix: fix the feign request url which gets store manage name

--- src/main/java/com/bit/lot/flower/auth/store/http/feign/StoreManagerNameFeignRequest.java
+++ src/main/java/com/bit/lot/flower/auth/store/http/feign/StoreManagerNameFeignRequest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "store-manager-name-request", url = "${service.store.domain}")
+@FeignClient(name = "store-manager-name-request", url = "${service.user.domain}")
 public interface StoreManagerNameFeignRequest {
   @GetMapping(value = "/stores/{storeManagerId}")
   ResponseEntity<StoreManagerNameDto> request(@PathVariable Long storeManagerId);
