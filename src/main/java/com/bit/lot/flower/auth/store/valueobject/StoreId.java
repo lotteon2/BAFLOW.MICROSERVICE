2910623620508e1d8ccea294833d10646070a7c7
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-21 09:30:15 +0900

    remove: remove unneccesary getter in storeId

--- src/main/java/com/bit/lot/flower/auth/store/valueobject/StoreId.java
+++ src/main/java/com/bit/lot/flower/auth/store/valueobject/StoreId.java
@@ -2,15 +2,10 @@ package com.bit.lot.flower.auth.store.valueobject;
 
 import com.bit.lot.flower.auth.common.valueobject.BaseId;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @NoArgsConstructor
 @SuperBuilder
 public class StoreId  extends BaseId<Long> {
-    @JsonProperty("storeId")
-    public Long getStoreIdValue() {
-        return this.getValue();
-    }
 }
