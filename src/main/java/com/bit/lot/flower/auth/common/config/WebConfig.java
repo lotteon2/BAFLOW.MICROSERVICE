bf4eb97a80b82bfa8c777dbbbe0d0463a900db50
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-01-10 18:07:18 +0900

    chore: add the origins

--- src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
+++ src/main/java/com/bit/lot/flower/auth/common/config/WebConfig.java
@@ -46,7 +46,9 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002",
-            "https://blooming.blooms.mall.stockey.kr")
+            "https://blooming.blooms.mall.stockey.kr",
+            "https://blooming.blooms.store.stockey.kr",
+            "https://blooming.blooms.admin.stockey.kr")
         .allowedMethods("*")
         .allowedHeaders("*")
         .exposedHeaders("*")
